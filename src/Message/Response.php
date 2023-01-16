f6377ab6a40db63d99ff84fdc8329396af36e641
Maksym Sharov <tomigun.ukraine@gmail.com>
2023-01-17 01:57:43 +0200

    [0.1.7]
    - fixed response

--- src/Message/Response.php
+++ src/Message/Response.php
@@ -4,24 +4,27 @@ declare(strict_types=1);
 
 namespace Omnipay\Amex\Message;
 
-use GuzzleHttp\Psr7\Stream;
 use Omnipay\Common\Message\RequestInterface;
+use Psr\Http\Message\StreamInterface;
 
 class Response extends \Omnipay\Common\Message\AbstractResponse
 {
-    private const SUCCESS_STATUS = 'SUCCESS';
+    private const SUCCESS_STATUSES = ['SUCCESS', 'OPERATING'];
 
     public function __construct(RequestInterface $request, $data)
     {
-        if ($data instanceof Stream) {
-            $data = $data->getContents();
+        if ($data instanceof StreamInterface) {
+            $body = $data->getContents();
+            $data->rewind();
+        } else {
+            $body = $data;
         }
 
-        parent::__construct($request, \json_decode($data, true));
+        parent::__construct($request, \json_decode($body, true));
     }
 
     public function isSuccessful(): bool
     {
-        return isset($this->data->status) && $this->data->status === self::SUCCESS_STATUS;
+        return isset($this->data['status']) && \in_array($this->data['status'], self::SUCCESS_STATUSES);
     }
 }
