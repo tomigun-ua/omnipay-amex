5ae0af46e664dcad9d897129c23ae9010071d015
Maksym Sharov <tomigun.ukraine@gmail.com>
2023-01-17 01:51:03 +0200

    [0.1.6]
    - fixed response

--- src/Message/Response.php
+++ src/Message/Response.php
@@ -4,9 +4,21 @@ declare(strict_types=1);
 
 namespace Omnipay\Amex\Message;
 
+use GuzzleHttp\Psr7\Stream;
+use Omnipay\Common\Message\RequestInterface;
+
 class Response extends \Omnipay\Common\Message\AbstractResponse
 {
     private const SUCCESS_STATUS = 'SUCCESS';
+
+    public function __construct(RequestInterface $request, $data)
+    {
+        if ($data instanceof Stream) {
+            $data = $data->getContents();
+        }
+
+        parent::__construct($request, \json_decode($data, true));
+    }
 
     public function isSuccessful(): bool
     {
